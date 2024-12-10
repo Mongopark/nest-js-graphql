a246502e61150b0aa43f39a8d5d7808be3db27f5
Mong Israel <mongsolomon@gmail.com>
2024-12-10 23:03:15 +0100

    env setup and deployment

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT || 5432);
+  await app.listen(process.env.APP_PORT || 3001);
 }
 bootstrap();
