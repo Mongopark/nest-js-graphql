429296625a04db3e5b19c561fb69aa22d0a7791f
Mong Israel <mongsolomon@gmail.com>
2024-12-10 22:25:45 +0100

    configured environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,28 +8,45 @@ import { UserSetting } from './graphql/models/UserSetting';
 import { UsersModule } from './users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { Department } from './graphql/models/Department';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
+console.log({
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.NODE_ENV === 'TEST'
+    ? process.env.POSTGRES_DB_TEST
+    : process.env.POSTGRES_DB,
+});
+
+
+
 
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'secretKey',
-      signOptions: { expiresIn: '1h' },
+      secret: process.env.TOKEN_SECRET,
+      signOptions: { expiresIn: process.env.TOKEN_EXPIRES_IN },
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql',
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '127.0.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'postgres',
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
       database:
         process.env.NODE_ENV === 'TEST'
-          ? 'graphql_tutorial_test'
-          : 'graphql_tutorial_test',
+          ? process.env.POSTGRES_DB_TEST
+          : process.env.POSTGRES_DB,
       entities: [User, UserSetting, Department],
       synchronize: true,
       logging: false,
